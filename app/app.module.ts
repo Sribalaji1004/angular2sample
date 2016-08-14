6dc78523cc77bba0dffd039a7191914ec31ef0b6
ursenix <ursenix@gmail.com>
2016-08-15 00:48:33 +0530

    Routing enabled.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { routing }        from './app.routing';
+
+import { HeroesComponent }  from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService }  from './hero.service';
+
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
-    imports:[BrowserModule],
-    declarations: [AppComponent],
-    bootstrap:[AppComponent]
+    
+    imports:[
+        BrowserModule,
+        FormsModule,
+        routing
+    ],
+    
+    declarations: [
+        AppComponent,
+        HeroesComponent,
+        DashboardComponent,
+        HeroDetailComponent
+    ],
+    
+    providers: [
+        HeroService
+    ],
+    
+    bootstrap:[ AppComponent ]
 })
 export class AppModule{}
